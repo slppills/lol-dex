9c36980be3b5d0955ede1f9ef7da83ca83a5646f
slppills <sunchaehoon2@naver.com>
2024-10-03 00:08:46 +0900

    add: server api

---
+++ src/utils/serverApi.ts
@@ -0,0 +1,43 @@
+type ChampionId = {
+  id: string;
+};
+
+export const fetchGetVersion = async () => {
+  try {
+    const res = await fetch("https://ddragon.leagueoflegends.com/api/versions.json");
+    const data: string[] = await res.json();
+    return data[0];
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const fetchChampionImg = async () => {
+  const version = await fetchGetVersion();
+  const baseUrl = `http://ddragon.leagueoflegends.com/cdn/${version}/img/champion/`;
+  return baseUrl;
+};
+
+export const fetchChampionList = async () => {
+  const version = await fetchGetVersion();
+  try {
+    const res = await fetch(`https://ddragon.leagueoflegends.com/cdn/${version}/data/ko_KR/champion.json`, {
+      next: { revalidate: 86400 },
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const fetchChampionDetail = async ({ id }: ChampionId) => {
+  const version = await fetchGetVersion();
+  try {
+    const res = await fetch(`https://ddragon.leagueoflegends.com/cdn/${version}/data/ko_KR/champion/${id}.json`);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
