17d037d480b769cc4cdba9c800ae53a1b7a25b64
slppills <sunchaehoon2@naver.com>
2024-10-05 23:53:38 +0900

    fix: mainLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,12 +19,10 @@ export default function RootLayout({
     <html lang="ko" className="h-[100%]">
       <body>
         <header className="fixed bg-[#0b141e] w-full flex py-5 px-10 items-center justify-between z-10">
-          <div className="flex items-center">
-            <Link href="/">
-              <Image src={MainLogo} alt="리그오브레전드 로고" width={50} height={50} />
-            </Link>
+          <Link href="/" className="flex items-center">
+            <Image src={MainLogo} alt="리그오브레전드 로고" width={50} height={50} />
             <h1 className="text-[#c79a3b] font-bold text-[1.5rem]">League Of Legends</h1>
-          </div>
+          </Link>
           <div className="flex gap-8">
             <Link href="/champions">
               <svg width="30" height="30" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -47,14 +45,15 @@ export default function RootLayout({
               </svg>
             </Link>
             <Link href="/rotation">
-            <svg width="30" height="30" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M10 3C6.1 3 1 1 1 1v5c0 10 9 13 9 13s9-3.1 9-13V1s-5.1 2-9 2ZM3 8c0-1 1.3-1 2-1 2 0 3 2 3 2s0 2-2 2c-1.8 0-3-1.9-3-3Zm7 8c-3.1 0-4-3-4-3s1.4 1 4 1c2.4 0 4-1 4-1s-.9 3-4 3Zm4-5c-2 0-2-2-2-2s1-2 3-2c.7 0 2 0 2 1 0 1.1-1.2 3-3 3Z"
-                fill="#CAAB67"
-              ></path>
-            </svg></Link>
+              <svg width="30" height="30" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M10 3C6.1 3 1 1 1 1v5c0 10 9 13 9 13s9-3.1 9-13V1s-5.1 2-9 2ZM3 8c0-1 1.3-1 2-1 2 0 3 2 3 2s0 2-2 2c-1.8 0-3-1.9-3-3Zm7 8c-3.1 0-4-3-4-3s1.4 1 4 1c2.4 0 4-1 4-1s-.9 3-4 3Zm4-5c-2 0-2-2-2-2s1-2 3-2c.7 0 2 0 2 1 0 1.1-1.2 3-3 3Z"
+                  fill="#CAAB67"
+                ></path>
+              </svg>
+            </Link>
           </div>
         </header>
         <div>{children}</div>
