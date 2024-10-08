95b449b818734b1cc67831a66d77e663009f5ffd
slppills <sunchaehoon2@naver.com>
2024-10-08 20:40:39 +0900

    update: rotationPage

--- src/app/(InfoList)/rotation/page.tsx
+++ src/app/(InfoList)/rotation/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Loading from "@/app/loading";
 import ChampionCard from "@/components/ChampionCard";
 import { ChampionType } from "@/types/Champion";
 import { fetchChampionImg, fetchChampionList } from "@/utils/serverApi";
@@ -7,25 +8,29 @@ import React, { useEffect, useState } from "react";
 
 const RotationPage = () => {
   const [rotationChampions, setRotationChampions] = useState<ChampionType[]>([]);
-  const [baseUrl, setBaseUrl] = useState<string>("");
+  const [baseUrl, setBaseUrl] = useState<string>();
   useEffect(() => {
     const fetchRotation = async () => {
-      const url = await fetchChampionImg();
-      setBaseUrl(url);
-      const rotationRes = await fetch("http://localhost:3000/api/rotation");
-      const { data } = await fetchChampionList();
-      const { freeChampionIds } = await rotationRes.json();
+      const [url, { freeChampionIds }, { data }] = await Promise.all([
+        fetchChampionImg(),
+        fetch("/api/rotation").then((res) => res.json()),
+        fetchChampionList(),
+      ]);
       const champions: ChampionType[] = Object.values(data);
       const filteredChampions = champions.filter((champion) => freeChampionIds.includes(Number(champion.key)));
       setRotationChampions(filteredChampions);
-      console.log(url);
+      setBaseUrl(url);
     };
     fetchRotation();
   }, []);
 
+  if (!baseUrl) {
+    return <Loading />;
+  }
+
   return (
     <>
-      <h1 className="text-red-600 font-bold text-[2rem] mb-[50px]">아이템 목록</h1>
+      <h1 className="text-red-600 font-bold text-[2rem] mb-[50px]">로테이션 챔피언</h1>
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
         {rotationChampions.map((champion: ChampionType) => (
           <ChampionCard key={champion.key} champion={champion} baseUrl={baseUrl} />
