4e51b5b94326fac8cde38b3c2f6c37820387c9b7
slppills <sunchaehoon2@naver.com>
2024-10-08 20:37:24 +0900

    update: router handler

--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -1,18 +1,9 @@
+import { fetchChampionRotation } from "@/utils/serverApi";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const apiKey = process.env.RIOT_API_KEY;
-
-  if (!apiKey) {
-    return NextResponse.json({ error: "API key is not set" }, { status: 500 });
-  }
-
   try {
-    const res = await fetch("https://kr.api.riotgames.com/lol/platform/v3/champion-rotations", {
-      headers: {
-        "X-Riot-Token": apiKey,
-      },
-    });
+    const res = await fetchChampionRotation();
 
     if (!res.ok) {
       throw new Error(`HTTP error! status: ${res.status}`);
